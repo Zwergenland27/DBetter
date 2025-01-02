ad0ed691ea7ff53c14fd05776d7e56ac6ef1a20c
Florian Königsmann <florian.koenigsmann@gmail.com>
2025-01-02 20:12:05 +0100

    Remove geo data from stop

--- DBetter.Contracts/Station.cs
+++ DBetter.Contracts/Station.cs
@@ -7,10 +7,4 @@ public class Station
     public String Id { get; set; } = null!;
 
     public String Name { get; set; } = null!;
-    
-    [JsonPropertyName("lat")]
-    public double Latitude { get; set; }
-    
-    [JsonPropertyName("lon")]
-    public double Longitude { get; set; }
 }
