cf04ed5fc5b7dfc2ff3086ea7c8ed5bc364eeba5
Florian Königsmann <florian.koenigsmann@gmail.com>
2025-02-20 17:20:01 +0100

    Add missing gzip decompression

--- DBetter.Infrastructure/BahnApi/DependencyInjection.cs
+++ DBetter.Infrastructure/BahnApi/DependencyInjection.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using DBetter.Infrastructure.BahnApi.Journey;
 using DBetter.Infrastructure.BahnApi.VehicleSequence;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,10 +10,18 @@ public static class DependencyInjection
     public static IServiceCollection AddBahnApi(this IServiceCollection services)
     {
         services.AddHttpClient<JourneyRepository>(
-            client => client.BaseAddress = new Uri("https://www.bahn.de/web/api/"));
+            client => client.BaseAddress = new Uri("https://www.bahn.de/web/api/"))
+            .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+            {
+                AutomaticDecompression = DecompressionMethods.GZip
+            });
         
         services.AddHttpClient<VehicleSequenceRepository>(
-            client => client.BaseAddress = new Uri("https://www.bahn.de/web/api/"));
+            client => client.BaseAddress = new Uri("https://www.bahn.de/web/api/"))
+            .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+            {
+                AutomaticDecompression = DecompressionMethods.GZip
+            });
         
         return services;
     }
