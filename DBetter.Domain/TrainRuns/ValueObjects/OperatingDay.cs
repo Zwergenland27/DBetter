ff14eff9baa1d8b1f7543435a60e51b1a6a4d2e1
Florian Königsmann <70534154+Zwergenland27@users.noreply.github.com>
2026-02-01 12:42:08 +0100

    Fix operating day parsing for single digit days

--- DBetter.Domain/TrainRuns/ValueObjects/OperatingDay.cs
+++ DBetter.Domain/TrainRuns/ValueObjects/OperatingDay.cs
@@ -4,6 +4,6 @@ public record OperatingDay(DateOnly Date)
 {
     public static OperatingDay Parse(string value)
     {
-        return new(DateOnly.ParseExact(value, "ddMMyy"));
+        return new(DateOnly.ParseExact(value.PadLeft(6, '0'), "ddMMyy"));
     }
 }
