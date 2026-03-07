3d97869bc4cbcc08a652720918aac68da52865fb
Florian Königsmann <70534154+Zwergenland27@users.noreply.github.com>
2026-03-07 13:30:09 +0100

    fix: Add migrations

---
+++ DBetter.Infrastructure/Migrations/20260307110139_AdditionalRouteAttributes.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DBetter.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdditionalRouteAttributes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Demand_FirstClass",
+                table: "RouteStops",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Demand_SecondClass",
+                table: "RouteStops",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Platform_Planned",
+                table: "RouteStops",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Platform_Real",
+                table: "RouteStops",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Platform_Type",
+                table: "RouteStops",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Demand_FirstClass",
+                table: "RouteStops");
+
+            migrationBuilder.DropColumn(
+                name: "Demand_SecondClass",
+                table: "RouteStops");
+
+            migrationBuilder.DropColumn(
+                name: "Platform_Planned",
+                table: "RouteStops");
+
+            migrationBuilder.DropColumn(
+                name: "Platform_Real",
+                table: "RouteStops");
+
+            migrationBuilder.DropColumn(
+                name: "Platform_Type",
+                table: "RouteStops");
+        }
+    }
+}
