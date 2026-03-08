f100d38b2d0e12e8008225d6370d5cd3bf16a4cd
Florian Königsmann <70534154+Zwergenland27@users.noreply.github.com>
2026-03-08 10:57:47 +0100

    feat: Store line number and line prefix in separte columns

--- DBetter.Domain/TrainCirculations/ValueObjects/LineNumber.cs
+++ DBetter.Domain/TrainCirculations/ValueObjects/LineNumber.cs
@@ -1,3 +1,24 @@
 namespace DBetter.Domain.TrainCirculations.ValueObjects;
 
-public record LineNumber(string Value);
+public record LineNumber
+{
+    public string Number { get; private init; }
+    
+    public string? Prefix { get; private init; }
+
+    private LineNumber(string number, string? prefix)
+    {
+        Number = number;
+        Prefix = prefix;
+    }
+    public static LineNumber Create(string value)
+    {
+        var splitted = value.Split(" ");
+        if (splitted.Length == 1)
+        {
+            return new LineNumber(splitted[0], null);
+        }
+        
+        return new LineNumber(splitted[1], splitted[0]);
+    }
+}
--- DBetter.Infrastructure/BahnDe/Shared/LineInformationFactory.cs
+++ DBetter.Infrastructure/BahnDe/Shared/LineInformationFactory.cs
@@ -22,22 +22,22 @@ public class LineInformationFactory(string produktGattung, string fullLineInform
         if (_category is TransportCategory.SuburbanTrain && splitLineInformation[0].StartsWith("S"))
         {
             _category = TransportCategory.SuburbanTrain;
-            _lineNumber = new LineNumber(splitLineInformation[0].Replace("S", "S "));
+            _lineNumber = LineNumber.Create(splitLineInformation[0].Replace("S", "S "));
         }
         //Boats etc.
         else if (_category is TransportCategory.Boat)
         {
-            _lineNumber = new LineNumber(fullLineInformation);
+            _lineNumber = LineNumber.Create(fullLineInformation);
         }
         //Remove "STR" Prefix for trams
         else if (_category is TransportCategory.Tram)
         {
-            _lineNumber = new LineNumber(splitLineInformation[1]);
+            _lineNumber = LineNumber.Create(splitLineInformation[1]);
         }
         //Remove "Bus" Prefix for buses
         else if(_category is TransportCategory.Bus)
         {
-            _lineNumber = new LineNumber(splitLineInformation[1]);
+            _lineNumber = LineNumber.Create(splitLineInformation[1]);
         }
         else if (_category is TransportCategory.Replacement && splitLineInformation[0] is "Bus")
         {
@@ -47,18 +47,18 @@ public class LineInformationFactory(string produktGattung, string fullLineInform
 
             if (number is "")
             {
-                _lineNumber = new LineNumber(lineNumberContent);
+                _lineNumber = LineNumber.Create(lineNumberContent);
             }
             else
             {
-                _lineNumber = new LineNumber($"{productClass} {number}");
+                _lineNumber = LineNumber.Create($"{productClass} {number}");
             }
         }
         //InterCity lines that can also be used with regional ticket
         else if (splitLineInformation.Length > 2 && splitLineInformation[0] is "RE" && splitLineInformation[1].StartsWith("IC"))
         {
             _category = TransportCategory.FastTrain;
-            _lineNumber = new LineNumber($"RE {new string(splitLineInformation[1].Where(char.IsDigit).ToArray())}");
+            _lineNumber = LineNumber.Create($"RE {new string(splitLineInformation[1].Where(char.IsDigit).ToArray())}");
             if (splitLineInformation.Length == 3 && splitLineInformation[2].Any(char.IsDigit))
             {
                 var serviceNumberContent = new string(splitLineInformation[2].Where(char.IsDigit).ToArray());
@@ -68,7 +68,7 @@ public class LineInformationFactory(string produktGattung, string fullLineInform
         //Trains without dedicated train number
         else if (splitLineInformation.Length == 2 && splitLineInformation[0].All(char.IsLetter))
         {
-            _lineNumber = new LineNumber($"{splitLineInformation[0]} {splitLineInformation[1]}");
+            _lineNumber = LineNumber.Create($"{splitLineInformation[0]} {splitLineInformation[1]}");
             
             //TODO: line number is sometimes the train number, like in ICEs or TGVs and should be extracted for a correct data model
             //This should not be done using static filters (like only for ices) because sometimes a normal train is also missing its line number
@@ -80,7 +80,7 @@ public class LineInformationFactory(string produktGattung, string fullLineInform
             var lineNumberContent = new string(splitLineInformation[0].Where(char.IsDigit).ToArray());
             var serviceNumberContent = new string(splitLineInformation[1].Where(char.IsDigit).ToArray());
             
-            _lineNumber = new LineNumber($"{productClassContent} {lineNumberContent}");
+            _lineNumber = LineNumber.Create($"{productClassContent} {lineNumberContent}");
             if (int.TryParse(serviceNumberContent, out var serviceNumber))
             {
                 _serviceNumber = new ServiceNumber(serviceNumber);
@@ -93,7 +93,7 @@ public class LineInformationFactory(string produktGattung, string fullLineInform
             var lineNumberContent = new string(splitLineInformation[1].Where(char.IsDigit).ToArray());
             var serviceNumberContent = new string(splitLineInformation[2].Where(char.IsDigit).ToArray());
             
-            _lineNumber = new LineNumber($"{productClassContent} {lineNumberContent}");
+            _lineNumber = LineNumber.Create($"{productClassContent} {lineNumberContent}");
             if (int.TryParse(serviceNumberContent, out var serviceNumber))
             {
                 _serviceNumber = new ServiceNumber(serviceNumber);
@@ -102,7 +102,7 @@ public class LineInformationFactory(string produktGattung, string fullLineInform
         //Catch all other cases
         else
         {
-            _lineNumber = new LineNumber(fullLineInformation);
+            _lineNumber = LineNumber.Create(fullLineInformation);
         }
         
         return new ServiceInformation(
